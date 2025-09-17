28136a4488213e3664796f2c4bc0d5f526c7bb29
Felix Eklund <felix.eklund.syne25lin@edu.tuc.sweden.se>
2025-09-17 12:51:21 +0200

    Update BMI calculator with user prompts and calculations
    Changed greeting message and added functionality to prompt
    for weight and height. Implemented BMI calculations using
    both standard and named arguments, displaying results to
    the user.

--- BmiCalculator/Program.cs
+++ BmiCalculator/Program.cs
@@ -20,7 +20,20 @@
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        Console.WriteLine("Välkommen till BMI-kalkylatorn!");
+            Console.Write("Ange din vikt: ");
+            double weight = Convert.ToDouble(Console.ReadLine());   
+            Console.Write("Ange din längd: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+           Console.WriteLine($"BMI (metriska enheter): {CalculateBMI(weight, height, "metric")}");
+           Console.WriteLine("--------------------------------");
+            double bmiWithNamedArgs = CalculateBMI(height: height, weight: weight); //väljer ordning själv
+            Console.WriteLine($"(annan ordning på height och weight  {bmiWithNamedArgs}");
+
+
+
+
+
         }
     }
 }
