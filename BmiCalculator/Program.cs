5b3633def4f35dc76e72029a8ee1bfde5534a909
Felix Eklund <felix.eklund.syne25lin@edu.tuc.sweden.se>
2025-09-17 12:05:29 +0200

    Add project files.

---
+++ BmiCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BmiCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
